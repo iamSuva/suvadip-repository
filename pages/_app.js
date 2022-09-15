6200e772ccd0f98264b76747bc011ee0da44a42b
Suvadip Maiti <suvadipm2002@gmail.com>
2022-09-15 15:31:52 +0530

    final commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import "../Components/navbar.js";
+import NavBar from '../Components/navbar.js';
+import Footer  from '../Components/footer';
+// import Blog from '../Components/Blogs';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return ( <>
+  <NavBar/>,
+  <Component {...pageProps} />,
+  
+  <Footer/>
+  </>)
 }
 
 export default MyApp
